0a45f6d23cd28817a8f4ad91d29b21349db07cd9
GZTY959 <132641421+GZTY959@users.noreply.github.com>
2025-04-03 02:44:29 +0800

    Create TPPrefsObserver.h

---
+++ TPPrefsObserver.h
@@ -0,0 +1,6 @@
+#import <Foundation/Foundation.h>
+
+@interface TPPrefsObserver : NSObject
+@property (nonatomic, assign) BOOL useiPadAppSwitchingAnimation;
+@end
+​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
